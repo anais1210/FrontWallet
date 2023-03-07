46643cc9aeb9c3d79024543313cd180c8a67afaa
Anais1210 <azhang@myges.fr>
2023-03-07 17:28:02 +0100

    add actionbutton

--- front-indexer/src/scenes/navbar/index.tsx
+++ front-indexer/src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 // import ActionButton from "@/shared/ActionButton";
 
 type Props = {
@@ -55,10 +56,9 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  {/* <ActionButton setSelectedPage={setSelectedPage}>
+                  <ActionButton setSelectedPage={setSelectedPage}>
                     Become a Member
-                  </ActionButton> */}
-                  <button>Become a Member</button>
+                  </ActionButton>
                 </div>
               </div>
             ) : (
