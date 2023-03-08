6dcda94b99b1c3db66dd69edc0d8ebf4d5856ade
Anais1210 <azhang@myges.fr>
2023-03-08 10:42:48 +0100

    home page done

--- front-indexer/src/App.tsx
+++ front-indexer/src/App.tsx
@@ -1,4 +1,9 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+// import OurClasses from "@/scenes/ourClasses";
+// import Benefits from "@/scenes/benefits";
+// import ContactUs from "@/scenes/contactUs";
+// import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -19,13 +24,15 @@ function App() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
   return (
-    <div className="app bg-gray-20 bg-gray-50">
+    <div className="app bg-gray-20">
       <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={SelectedPage.Home} />
     </div>
   );
 }
